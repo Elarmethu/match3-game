c9d4eb3a16f8a9f78f7006746f3567e5506e87bd
Ignat Karelov-Suslov <elarmethu@gmail.com>
2022-09-02 15:24:45 +0300

    Create PrefsKey.cs

---
+++ Assets/Match3/Scripts/PrefsKey.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// This class repsonsibele about get information about prefs keys.
+/// </summary>
+public static class PrefsKey
+{
+    public static string audio = "M3_audio";
+    public static string grid = "M3_grid";
+}
